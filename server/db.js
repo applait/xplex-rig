3c3dcdfbdaf44317d18a62d7d12aefbe8143f52e
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-08-07 02:40:30 +0530

    Add DB integration script with basic User model
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- server/db.js
+++ server/db.js
@@ -1,13 +1,49 @@
 /**
- * Database wrapper
+ * Database wrapper. Set up connections and load models
+ *
+ * Call `.connect()` once and use the module for the rest of your life
  */
 
 const debug = require('util').debuglog('DB')
+const Sequelize = require('sequelize')
+const { readdirSync } = require('fs')
+const { join } = require('path')
 
-let connect = (postgresURL) => {
+let DB = {}
+
+/**
+ * Method to call to initiate DB connection
+ *
+ * @param {string} postgresURL - Full URL to PostgreSQL instance
+ * @return {Sequelize} Connected Sequelize instance
+ */
+let connect = postgresURL => {
   debug(`[DB] Connecting to DB at ${postgresURL}`)
+  let s = new Sequelize(postgresURL, {
+    dialect: 'postgres',
+    logging: i => debug(i)
+  })
+
+  readdirSync(join(__dirname, 'models'))
+    .filter(function (file) {
+      return (file.indexOf('.') !== 0) && (file !== 'index.js')
+    })
+    .forEach(function (file) {
+      const model = s.import(join(__dirname, 'models', file))
+      DB[model.name] = model
+    })
+
+  Object.keys(DB).forEach(function (modelName) {
+    if ('associate' in DB[modelName]) {
+      DB[modelName].associate(DB)
+    }
+  })
+
+  DB.sequelize = s
+  DB.Sequelize = Sequelize
 }
 
 module.exports = {
-  connect: connect
+  connect: connect,
+  DB: DB
 }
