8fe2d2a3f81699b0001a6f12598ff052bf2d5e4c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-09-10 17:35:55 +0530

    Add model and postgres connections with relations
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

---
+++ server/datastore/multistream.go
@@ -0,0 +1,28 @@
+package datastore
+
+import "time"
+
+// RTMPStream stores information of multi-streaming keys for users
+type RTMPStream struct {
+	ID          int
+	Key         string `sql:",unique,notnull"`
+	IsActive    bool   `sql:",notnull,default:false"`
+	IsStreaming bool   `sql:",notnull,default:false"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+
+	UserID     int         // RTMPStream belongsTo User
+	RTMPPushes []*RTMPPush // RTMPStream hasMany RTMPPush
+}
+
+// RTMPPush store configuration information of RTMP ingestion services
+// to push to
+type RTMPPush struct {
+	ID       int
+	Service  string `sql:",notnull"`
+	Key      string `sql:",notnull"`
+	Server   string `sql:",notnull,type:text default 'default'"`
+	IsActive bool   `sql:",notnull,default:false"`
+
+	RTMPStreamID int // RTMPPush belongsTo RTMPStream
+}
