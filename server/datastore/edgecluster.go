60c4cd3ff2dfdf537be43ad2939e2e97c3fd6b59
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-09-10 18:59:54 +0530

    Add EdgeCluster and AgentNode models
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

---
+++ server/datastore/edgecluster.go
@@ -0,0 +1,21 @@
+package datastore
+
+// EdgeCluster stores DNS and location information for edge clusters
+type EdgeCluster struct {
+	ID        int
+	PublicDNS string `sql:",notnull,unique"`
+	Region    string `sql:",notnull"`
+	Provider  string
+	IsActive  bool `sql:",notnull,default:false"`
+
+	AgentNodes []*AgentNode // EdgeCluster hasMany AgentNode
+}
+
+// AgentNode stores hostname and information
+type AgentNode struct {
+	ID       int
+	Hostname string `sql:",notnull,unique"`
+	IsActive bool   `sql:",notnull,default:false"`
+
+	EdgeClusterID int // AgentNode belongsTo EdgeCluster
+}
