dfee80cd7036dc388d32699bf1aa36431496be0d
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-09-14 20:16:10 +0530

    Remove dummy methods from main()
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- server/server.go
+++ server/server.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/applait/xplex-rig/server/datastore"
 	"github.com/gorilla/mux"
 )
 
@@ -14,15 +12,6 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	connuri := "postgres://xplex:1234@localhost/xplex_rig_dev?sslmode=disable"
-	db, err := datastore.ConnectPG(connuri)
-	if err != nil {
-		log.Fatalf("Error connecting to DB. Reason: %s", err)
-	}
-	err = datastore.CreateSchema(db)
-	if err != nil {
-		log.Fatalf("Error creating Postgres schema. Reason: %s", err)
-	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	http.ListenAndServe(":8081", r)
