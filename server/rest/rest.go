8e9db29df8c7889b4554a0e35f252e99ad09209e
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-09-16 18:15:09 +0530

    Setup REST response structure
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- server/rest/rest.go
+++ server/rest/rest.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/applait/xplex-rig/server/config"
@@ -8,10 +9,29 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Res defines response structure
+type Res struct {
+	Msg     string      `json:"msg"`
+	Status  int         `json:"status"`
+	Payload interface{} `json:"payload,omitempty"`
+}
+
+// Send sends out a JSON response
+func (r Res) Send(w http.ResponseWriter) (int, error) {
+	m, err := json.Marshal(r)
+	if err != nil {
+		return 0, err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(r.Status)
+	return w.Write(m)
+}
+
 // Start bootstraps the REST API
 func Start(db *pg.DB, config *config.Config) *mux.Router {
 	r := mux.NewRouter()
-	users(r.PathPrefix("/users").Subrouter())
+	users(r.PathPrefix("/users").Subrouter().StrictSlash(true))
+	r.HandleFunc("/", home)
 	return r
 }
 
@@ -19,4 +39,17 @@ func users(r *mux.Router) {
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Users!\n"))
 	})
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	o := Res{
+		Msg:    "xplex-rig HTTP API v1",
+		Status: 200,
+		Payload: []string{
+			"GET /",
+			"GET /users",
+			"GET /streams",
+		},
+	}
+	o.Send(w)
 }
