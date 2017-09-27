d7896dcf3a0b70b87faf30a80ec115e7685dda92
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-09-28 02:57:04 +0530

    Implement stream update key and use UUID primary key
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- models/multistream.go
+++ models/multistream.go
@@ -8,49 +8,21 @@ import (
 
 	"github.com/applait/xplex-rig/token"
 	"github.com/go-pg/pg"
+	uuid "github.com/satori/go.uuid"
 )
 
 // MultiStream stores information of multi-streaming keys for users
 type MultiStream struct {
-	ID          int
-	Key         string `sql:",unique,notnull"`
-	IsActive    bool   `sql:",notnull,default:false"`
-	IsStreaming bool   `sql:",notnull,default:false"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID            uuid.UUID `sql:",pk,type:uuid"`
+	Key           string    `sql:",unique,notnull"`
+	IsActive      bool      `sql:",notnull,default:false"`
+	IsStreaming   bool      `sql:",notnull,default:false"`
+	UserAccountID uuid.UUID `sql:",type:uuid"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 
-	UserID             int                  // MultiStream belongsTo User
+	UserAccount        *UserAccount         // MultiStream belongsTo UserAccount
 	MultiStreamConfigs []*MultiStreamConfig // MultiStream hasMany MultiStreamConfigs
-}
-
-func genKey(u *User) (string, error) {
-	t, err := token.NewUserToken(u.ID, fmt.Sprintf("xplex://%s@%s", u.Username, time.Now()), u.Password)
-	if err != nil {
-		return "", err
-	}
-	s1 := sha1.New()
-	s1.Write([]byte(t))
-	return hex.EncodeToString(s1.Sum(nil)), nil
-}
-
-// Create generates Key and inserts MultiStream row in DB
-func (m *MultiStream) Create(db *pg.DB) error {
-	u := User{ID: m.UserID}
-	err := u.Find(db)
-	if err != nil {
-		return err
-	}
-	if m.Key, err = genKey(&u); err != nil {
-		return err
-	}
-	return m.Insert(db)
-}
-
-// Insert inserts new row for MultiStream after adding timestamps
-func (m *MultiStream) Insert(db *pg.DB) error {
-	m.CreatedAt = time.Now()
-	m.UpdatedAt = time.Now()
-	return db.Insert(m)
 }
 
 // MultiStreamConfig stores configuration information of RTMP ingestion services
@@ -62,5 +34,84 @@ type MultiStreamConfig struct {
 	Server   string `sql:",notnull,default:'default'"`
 	IsActive bool   `sql:",notnull,default:false"`
 
-	MultiStreamID int // MultiStreamConfig belongsTo MultiStream
+	MultiStreamID uuid.UUID    `sql:",type:uuid"`
+	MultiStream   *MultiStream // MultiStreamConfig belongsTo MultiStream
+}
+
+// genKey generates multistream keys
+func genKey(userid uuid.UUID) (string, error) {
+	k := uuid.NewV4().String()
+	t, err := token.NewUserToken(userid, fmt.Sprintf("xplex://%s@%s", userid, k), k)
+	if err != nil {
+		return "", err
+	}
+	s1 := sha1.New()
+	s1.Write([]byte(t))
+	return hex.EncodeToString(s1.Sum(nil)), nil
+}
+
+// Find a MultiStream
+func (m *MultiStream) Find(db *pg.DB) error {
+	q := db.Model(m)
+	if m.ID != uuid.Nil {
+		q.Where("multi_stream.id = ?", m.ID)
+	}
+	if m.Key != "" {
+		q.Where("multi_stream.key = ?", m.Key)
+	}
+	if m.IsActive {
+		q.Where("multi_stream.is_active = ?", true)
+	}
+	if m.IsStreaming {
+		q.Where("multi_stream.is_streaming = ?", true)
+	}
+	if m.UserAccountID != uuid.Nil {
+		q.Where("user_account.id = ?", m.UserAccountID)
+		q.Column("UserAccount.id", "UserAccount.username", "UserAccount.is_active")
+	}
+	return q.First()
+}
+
+// Create generates Key and inserts MultiStream row in DB
+func (m *MultiStream) Create(db *pg.DB) error {
+	u := UserAccount{ID: m.UserAccountID}
+	err := u.Find(db)
+	if err != nil {
+		return err
+	}
+	if m.Key, err = genKey(m.UserAccountID); err != nil {
+		return err
+	}
+	return m.Insert(db)
+}
+
+// Update current multistream in DB
+func (m *MultiStream) Update(db *pg.DB) (bool, error) {
+	m.UpdatedAt = time.Now()
+	res, err := db.Model(m).Update()
+	if err != nil {
+		return false, err
+	}
+	if res.RowsAffected() == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
+// UpdateKey updates
+func (m *MultiStream) UpdateKey(db *pg.DB) (bool, error) {
+	key, err := genKey(m.UserAccountID)
+	if err != nil {
+		return false, err
+	}
+	m.Key = key
+	return m.Update(db)
+}
+
+// Insert inserts new row for MultiStream after adding timestamps
+func (m *MultiStream) Insert(db *pg.DB) error {
+	m.CreatedAt = time.Now()
+	m.UpdatedAt = time.Now()
+	m.ID = uuid.NewV4()
+	return db.Insert(m)
 }
--- rest/stream_handler.go
+++ rest/stream_handler.go
@@ -3,20 +3,21 @@ package rest
 import (
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/applait/xplex-rig/config"
 	"github.com/applait/xplex-rig/models"
 	"github.com/applait/xplex-rig/token"
 	"github.com/go-pg/pg"
 	"github.com/gorilla/mux"
+	uuid "github.com/satori/go.uuid"
 )
 
 // StreamHandler providers handler for `/streams` HTTP API
 func StreamHandler(r *mux.Router, db *pg.DB, conf *config.Config) {
 	r.HandleFunc("/", streamhome).Methods("GET")
 	rpost := r.Methods("POST").Subrouter()
-	rpost.Handle("/create", newChain(auth(conf.Server.JWTSecret, "user")).use(streamCreate(db)))
+	rpost.Handle("/", newChain(auth(conf.Server.JWTSecret, "user")).use(streamCreate(db)))
+	rpost.Handle("/key", newChain(required("streamID"), auth(conf.Server.JWTSecret, "user")).use(streamUpdateKey(db)))
 }
 
 func streamhome(w http.ResponseWriter, r *http.Request) {
@@ -24,10 +25,10 @@ func streamhome(w http.ResponseWriter, r *http.Request) {
 		Msg:    "Streams API",
 		Status: 200,
 		Payload: []string{
-			"POST /create",
-			"POST /config/new",
+			"POST /",
 			"GET /config",
-			"POST /updateKey",
+			"POST /config",
+			"POST /key",
 			"GET /list",
 		},
 	}
@@ -35,26 +36,53 @@ func streamhome(w http.ResponseWriter, r *http.Request) {
 }
 
 type streamCreateRes struct {
-	streamID  int
-	streamKey string
-	active    bool
-	user      int
+	StreamID  uuid.UUID `json:"streamID"`
+	StreamKey string    `json:"streamKey"`
+	Active    bool      `json:"active"`
+	User      uuid.UUID `json:"user"`
 }
 
 // streamCreate creates new multistream entry for user
 func streamCreate(db *pg.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		claims := r.Context().Value(ctxClaims).(*token.Claims)
-		uid, _ := strconv.Atoi(claims.Issuer)
 		m := models.MultiStream{
-			UserID: uid,
+			UserAccountID: uuid.FromStringOrNil(claims.Issuer),
 		}
 		if err := m.Create(db); err != nil {
 			log.Printf("Error creating multistream. Reason: %s", err)
 			errorRes(w, "Error creating multistream", http.StatusBadRequest)
 			return
 		}
-		log.Printf("New multistream created for user %d", uid)
-		success(w, "Stream created.", http.StatusOK, streamCreateRes{m.ID, m.Key, m.IsActive, m.UserID})
+		log.Printf("New multistream created. Stream ID: %s, user ID: %s", m.ID, m.UserAccountID)
+		success(w, "Stream created.", http.StatusOK, streamCreateRes{m.ID, m.Key, m.IsActive, m.UserAccountID})
+	}
+}
+
+type streamUpdateRes struct {
+	StreamID  uuid.UUID `json:"streamID"`
+	StreamKey string    `json:"streamKey"`
+}
+
+// streamUpdate updates multistreaming key for given multistream
+func streamUpdateKey(db *pg.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		claims := r.Context().Value(ctxClaims).(*token.Claims)
+		m := models.MultiStream{
+			ID:            uuid.FromStringOrNil(r.FormValue("streamID")),
+			UserAccountID: uuid.FromStringOrNil(claims.Issuer),
+		}
+		if err := m.Find(db); err != nil {
+			log.Printf("Error updating multistream key. Reason: %s", err)
+			errorRes(w, "Invalid stream details.", http.StatusBadRequest)
+			return
+		}
+		if ok, err := m.UpdateKey(db); !ok {
+			log.Printf("Error updating multistream key. Reason: %s", err)
+			errorRes(w, "Error updating multistream key.", http.StatusBadRequest)
+			return
+		}
+		log.Printf("Updated streamkey for stream %s, user %s", m.ID, m.UserAccountID)
+		success(w, "Stream key updated.", http.StatusOK, streamUpdateRes{m.ID, m.Key})
 	}
 }
