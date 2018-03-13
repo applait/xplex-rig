984d479cbb0ece2160f06250514ced51396d41c9
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2018-03-13 22:17:34 +0530

    Add stream fetch and key update functions
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- stream/stream.go
+++ stream/stream.go
@@ -2,6 +2,7 @@ package stream
 
 import (
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"time"
 
@@ -13,6 +14,42 @@ import (
 func createStreamKey(userID uuid.UUID, streamID uuid.UUID) string {
 	uid := uuid.NewV5(userID, fmt.Sprintf("%s-%s", streamID.String(), time.Now()))
 	return base64.RawURLEncoding.EncodeToString(uid.Bytes())
+}
+
+// GetStreamByID returns a stream's information given its ID
+func GetStreamByID(streamID uuid.UUID) (common.Stream, error) {
+	query := `
+    select
+      m.id, m.stream_key, m.is_active, m.is_streaming
+    from multi_streams m
+      where m.id = $1;
+  `
+	var s common.Stream
+	err := common.DB.QueryRow(query, streamID).Scan(
+		&s.ID, &s.StreamKey, &s.IsActive, &s.IsStreaming,
+	)
+	if err != nil {
+		return s, err
+	}
+	return s, nil
+}
+
+// GetStreamByStreamKey returns a stream's information given a stream key
+func GetStreamByStreamKey(streamKey string) (common.Stream, error) {
+	query := `
+    select
+      m.id, m.stream_key, m.is_active, m.is_streaming
+    from multi_streams m
+      where m.stream_key = $1;
+  `
+	var s common.Stream
+	err := common.DB.QueryRow(query, streamKey).Scan(
+		&s.ID, &s.StreamKey, &s.IsActive, &s.IsStreaming,
+	)
+	if err != nil {
+		return s, err
+	}
+	return s, nil
 }
 
 // CreateStream creates a new stream for a given user ID
@@ -39,4 +76,22 @@ func CreateStream(userID uuid.UUID) (common.Stream, error) {
 		return s, err
 	}
 	return s, nil
+}
+
+// ChangeStreamKey creates a new stream key for a given stream of a given user
+func ChangeStreamKey(streamID uuid.UUID, userID uuid.UUID) (string, error) {
+	query := `
+    update multi_streams
+      set stream_key = $1
+    where id = $2;
+  `
+	newKey := createStreamKey(userID, streamID)
+	res, err := common.DB.Exec(query, newKey, streamID)
+	if err != nil {
+		return newKey, err
+	}
+	if affected, err := res.RowsAffected(); err != nil || affected != 1 {
+		return newKey, errors.New("Unable to update stream key")
+	}
+	return newKey, nil
 }
